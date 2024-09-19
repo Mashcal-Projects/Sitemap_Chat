91f0be12a3faaff96a2b3bb491bafa860786cc70
Mashcal-Projects <157454579+Mashcal-Projects@users.noreply.github.com>
2024-09-19 15:33:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ def generate_response(prompt, diagram_data=None):
         
 def load_db(file_path):
     # Load the questions and diagrams from a CSV file
-    df = pd.read_csv(file_path, encoding='utf-8')
+    df = pd.read_csv(file_path)
     return df
 
 def user_input(user_question, diagram_data=None, tags=None, link=None):
